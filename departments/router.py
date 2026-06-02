047331340369de71bfb7c09220f2ccd067c3bc7c
sebastian <sebastiangeorge004@gmail.com>
2026-06-02 11:35:11 +0530

    feat: added RBAC to departments

--- departments/router.py
+++ departments/router.py
@@ -5,16 +5,20 @@ from database import get_db
 from fastapi import Depends, status, APIRouter
 from departments import service
 from departments.schemas import DepartmentCreate, DepartmentUpdate, DepartmentResponse, DepartmentResponseId
-from auth.dependencies import get_current_user
+from auth.dependencies import get_current_user, require_role
 from auth.schemas import TokenPayload
+from models.employee import EmployeeRole
 
 router = APIRouter(prefix="/department", tags=["Departments"])
 
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=DepartmentResponse)
-async def create_department(
-    body: DepartmentCreate, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)
-):
+@router.post(
+    "",
+    status_code=status.HTTP_201_CREATED,
+    response_model=DepartmentResponse,
+    dependencies=[Depends(require_role(EmployeeRole.HR))],
+)
+async def create_department(body: DepartmentCreate, db: AsyncSession = Depends(get_db)):
     department = await service.create(db, body.name, body.employee_ids)
     return department
 
@@ -43,20 +47,24 @@ async def get_department_by_id(
     return result
 
 
-@router.put("/{department_id}", response_model=DepartmentResponse)
+@router.put(
+    "/{department_id}", response_model=DepartmentResponse, dependencies=[Depends(require_role(EmployeeRole.HR))]
+)
 async def update_department(
     department_id: int,
     body: DepartmentUpdate,
     db: AsyncSession = Depends(get_db),
-    _current_user: TokenPayload = Depends(get_current_user),
 ):
     result = await service.update_department(department_id, body.name, body.employee_ids, db)
     return result
 
 
-@router.delete("/{department_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    "/{department_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(require_role(EmployeeRole.HR))]
+)
 async def soft_delete_department(
-    department_id: int, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)
+    department_id: int,
+    db: AsyncSession = Depends(get_db),
 ):
     await service.soft_delete_department(department_id, db)
     return {"message": "Department soft deleted"}
